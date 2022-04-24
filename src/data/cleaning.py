86daea6bbea52b1a9c0d7338b3634c71f6eb1983
Ryan Keen <ryankeen19@gmail.com>
2022-04-23 23:28:08 -0700

    4/23 Additions
    Folder additions, created .py files, cleaned up project structure

---
+++ src/data/cleaning.py
@@ -0,0 +1,51 @@
+from data_import import raw_data
+import pandas as pd
+
+def clean_transform(raw_data):
+    #Drop duplicates
+    df = raw_data.drop_duplicates()
+    
+    #Change data types, column names, percentages to decimals
+    df = raw_data.astype({'season': str, 'starter': str, 'did_not_play': str, 'is_inactive': str})
+    df['game_date'] = pd.to_datetime(df['game_date'], infer_datetime_format = True)
+    df = df.rename(columns = {'Team_efg_pct': 'Team_efg_rate', 'Team_tov_pct' : 'Team_tov_rate', 
+                              'Team_orb_pct': 'Team_orb_rate', 'Opponent_efg_pct': 'Opponent_efg_rate', 
+                              'Opponent_tov_pct': 'Opponent_tov_rate', 'Opponent_orb_pct': 'Opponent_orb_rate',
+                             'ts_pct': 'ts_rate', 'efg_pct': 'efg_rate', 'ts_pct': 'ts_rate', 'orb_pct': 'orb_rate', 
+                             'drb_pct': 'drb_rate', 'trb_pct': 'trb_rate', 'ast_pct': 'ast_rate', 
+                              'stl_pct': 'stl_rate','blk_pct': 'blk_rate', 'tov_pct': 'tov_rate', 
+                              'usg_pct': 'usg_rate', 'ft_pct': 'ft_rate', 'fg_pct': 'fg_rate', 'fg3_pct': 'fg3_rate'})
+    df[['Team_tov_rate', 'Team_orb_rate', 'Opponent_orb_rate', 'Opponent_tov_rate', 'orb_rate', 
+        'drb_rate', 'trb_rate', 'ast_rate', 'stl_rate', 'blk_rate', 'tov_rate', 'usg_rate']] = df[['Team_tov_rate', 'Team_orb_rate', 'Opponent_orb_rate', 'Opponent_tov_rate', 'orb_rate', 
+        'drb_rate', 'trb_rate', 'ast_rate', 'stl_rate', 'blk_rate', 'tov_rate', 'usg_rate']]/100
+    
+    #Drop rows for games that went to overtime
+    df = df[df['OT'] == 0]
+    
+    #Remove unnecessary columns
+    df = df.drop(['OT', 'player', 'mp', 'double_double', 'triple_double', 'active_position_minutes', 
+                   'PG%', 'SG%', 'SF%', 'C%', 'Inactives', 'pf', 'is_inactive', 'PF%',
+                    'last_60_minutes_per_game_bench', 'last_60_minutes_per_game_starting',
+                    'DKP', 'FDP', 'SDP', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov', 'ts'], axis = 1, 
+                               inplace = False)
+    
+    #New col concatenate game_id and player_id
+    df.loc[:, 'game_id_player_id'] = df['game_id'].astype('str') + df['player_id'].astype('str')
+    
+    #Dedupe new column, move to first
+    duplicates = df.duplicated(['game_id_player_id'], keep='first')
+    df = df[~duplicates]
+    new_id = df.pop('game_id_player_id')
+    df.insert(0, 'game_id_player_id', new_id)
+    
+    #Sort by player, date
+    df = df.sort_values(by = ['player_id', 'game_date'])
+    
+    #Reset index
+    df.reset_index(drop = True, inplace = True)
+
+    df_out = df
+    df_out.to_csv("/Users/ryankeen/Personal_Projects/NBA_Betting/data/cleaned/cleaned_data.csv", index = False)
+
+clean_transform(raw_data)
+
---
+++ src/data/data_import.py
@@ -0,0 +1,7 @@
+"""
+@author: Ryan
+"""
+
+import pandas as pd
+
+raw_data = pd.read_csv('/Users/ryankeen/Personal_Projects/NBA_Betting/data/raw/19-20.csv')
